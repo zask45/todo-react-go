fd0a7cf539e3cbf0d2a294003314171e958e9068
zask45 <zaschakeysha@gmail.com>
2025-01-15 00:51:18 +0700

    add fetcher function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 
-const ENDPOINT = 'https://localhost:4000'
+const ENDPOINT = 'http://localhost:4000'
 
 const fetcher = (url: string) => 
   fetch(`${ENDPOINT}/${url}`).then(r => r.json())
 
 function App() {
-  <></>
+  
 }
 
 export default App

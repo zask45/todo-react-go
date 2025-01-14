765c37e298178c81801db9e686f61d3d19e048a3
zask45 <zaschakeysha@gmail.com>
2025-01-15 00:52:03 +0700

    use SWR

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
+import useSWR from 'swr'
 import './App.css'
+import { Box, MantineProvider } from '@mantine/core'
 
 const ENDPOINT = 'http://localhost:4000'
 
@@ -6,7 +8,13 @@ const fetcher = (url: string) =>
   fetch(`${ENDPOINT}/${url}`).then(r => r.json())
 
 function App() {
-  
+  const {data, mutate} = useSWR('api/todos', fetcher)
+
+  return <MantineProvider>
+    <Box>
+      {JSON.stringify(data)}
+    </Box>
+  </MantineProvider>
 }
 
 export default App

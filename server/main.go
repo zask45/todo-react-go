786b54b7b09c7becbdcd72e86e5f282099855357
zask45 <zaschakeysha@gmail.com>
2025-01-14 23:05:34 +0700

    add POST method

--- server/main.go
+++ server/main.go
@@ -11,7 +11,7 @@ type Todo struct {
 	ID    int    `json:"id"`
 	Title string `json:"title"`
 	Done  bool   `json:"done"`
-	Body  int    `json:"body"`
+	Body  string `json:"body"`
 }
 
 func main() {
@@ -19,8 +19,24 @@ func main() {
 
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":4000"))
